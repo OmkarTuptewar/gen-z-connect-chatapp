1b0ad8d8a19be6e1cfec731c566fae3269300ea5
OmkarTuptewar <omkarsanjay2004@gmail.com>
2024-04-23 13:05:50 +0530

    responsive app is ready

--- frontend/src/components/authentication/Login.jsx
+++ frontend/src/components/authentication/Login.jsx
@@ -9,9 +9,15 @@ import { useHistory } from "react-router-dom";
 
 
 const Login = () => {
+
+
+
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
   const toast = useToast();
+
+
+  
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [loading, setLoading] = useState(false);
